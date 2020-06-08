92a6ab3dcdbfd9f5330c80cb3ed4635ecfacfc93
archana-ragam <66559726+archana-ragam@users.noreply.github.com>
2020-06-08 12:04:05 +0530

    Code on screen

---
+++ Postivenum.py
@@ -0,0 +1,12 @@
+#print postive numbers in list1
+list1=[12,-7,5,64,-14]
+for mum in list1:
+    if num>=0:
+       print(num)
+ 
+
+#print postive numbers in list2
+list2=[12,14,-95,3]
+for i in list2:
+   if i>=0:
+      print(i)
